3241bb66baed12389677494ec4e51f561cfcd4b9
damlaYasarr <damlayasar40@gmail.com>
2024-02-01 19:05:53 +0300

    update port

---
+++ README.md
@@ -0,0 +1,7 @@
+- 
+
+
+- dependencies: 
+
+
+python, openai, 
--- server/index.py
+++ server/index.py
@@ -35,4 +35,4 @@ def soru():
 
 # Bu satırı ekledik
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 8080))
