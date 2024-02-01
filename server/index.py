8e0ffd8ef2f17366636a903814ef4e623f887d02
damlaYasarr <damlayasar40@gmail.com>
2024-02-01 20:51:54 +0300

    deleted react

--- server/index.py
+++ server/index.py
@@ -1,37 +1,43 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import openai
 from dotenv import load_dotenv
 import os
 from flask_cors import CORS
 
-load_dotenv()
+load_dotenv(dotenv_path='~/server/.env')
+
 
 API_KEY = os.getenv("API_KEY")
 app = Flask(__name__)
 CORS(app)
 
-openai.api_key = API_KEY
+ 
+openai.api_key = API_KEY   
 
-@app.route('/', methods=['POST'])
-def ask_question():
-    
-    data = request.get_data(as_text=True)
-   
-    user_response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful nutritionist."},
-            {"role": "user", "content": data}
-        ],
-        temperature=0.7,
-        max_tokens=300
-    )
+@app.route('/', methods=['POST','GET'])
+def home():
+    if request.method == 'POST':
+        
+        data = request.get_data(as_text=True)
 
-    assistant_response = user_response['choices'][0]['message']['content']
-    return assistant_response
-@app.route('/ask')
-def soru():
-    return "burası backend"
+        user_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful nutritionist."},
+                {"role": "user", "content": data}
+            ],
+            temperature=0.7,
+            max_tokens=300
+        )
+        assistant_response = user_response['choices'][0]['message']['content']
+        
+        return assistant_response
+
+    else:
+        
+        return render_template('main.html')
+
+
 
 # Bu satırı ekledik
 if __name__ == '__main__':
